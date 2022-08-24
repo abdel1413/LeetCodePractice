e0c824c6190197d180989e57f599d06f35f16d96
Your Name <you@example.com>
2022-08-24 07:53:20 -0400

    add some comment

--- Practice/Search_Insert.js
+++ Practice/Search_Insert.js
@@ -2,6 +2,17 @@
  *
  * @param {*} numsArray
  * @param {*} target
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
  * NOTE here we need to use binary search since
  * the Time complexity is O(log(n))
  */
